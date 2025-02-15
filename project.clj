861507aa136a57106bef6d2a68301d99a4e4d345
Juuso <juuso.melentjeff@gmail.com>
2025-02-15 13:40:47 +0200

    add backend dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,13 @@
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.postgresql/postgresql "42.7.5"]
                  [com.github.seancorfield/next.jdbc "1.3.994"]
-                 [migratus "1.6.3"]]
+                 [migratus "1.6.3"]
+                 [ring/ring-core "1.13.0"]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [ring/ring-json "0.5.1"]
+                 [compojure "1.7.1"]
+                 [com.layerware/hugsql "0.5.3"]
+                 [com.github.seancorfield/honeysql "2.6.1270"]]
   :plugins [[migratus-lein "0.7.3"]]
   :migratus {:store :database
              :migration-dir "migrations"
