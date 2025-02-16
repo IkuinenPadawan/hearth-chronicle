00ba0a010fb8ae4a6406aaa1a9126fc55bc585cc
Juuso <juuso.melentjeff@gmail.com>
2025-02-16 12:59:42 +0200

    feat: add initial Fullcalendar.js component

--- src/hearth_chronicle/frontend.cljs
+++ src/hearth_chronicle/frontend.cljs
@@ -1,11 +1,33 @@
 (ns hearth-chronicle.frontend
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            ["@fullcalendar/core" :refer [Calendar]]
+            ["@fullcalendar/daygrid" :default dayGridPlugin]
+            ["@fullcalendar/interaction" :default interactionPlugin]))
+
+(defn handle-date-click [info]
+  (js/alert (str "date clicked" (.-dateStr info))))
+
+(defn calendar-component []
+  (r/create-class
+    {:component-did-mount
+     (fn [this]
+       (let [calendar-el (.querySelector js/document "#calendar")
+             calendar    (Calendar. calendar-el
+                                    (clj->js {:plugins [dayGridPlugin interactionPlugin]
+                                              :initialView "dayGridMonth"
+                                              :dateClick handle-date-click
+                                              :events [{:title "Event 1" :start "2025-02-20"}
+                                                       {:title "Event 2" :start "2025-02-25"}]}))]
+         (.render calendar)))
+     :reagent-render
+     (fn []
+       [:div#calendar])}))
 
 (defn home-page []
   [:div
    [:h1 "Welcome to Hearth Chronicle"]
-   [:p "Here will be the chronicle."]])
+   [calendar-component]])
 
 (defn init []
   (rdom/render [home-page] (.getElementById js/document "app")))
